a4da27f511822a67e3a9ab8549c34debc340aadb
Branden <yp0975eu@go.minneapols.edu>
2016-05-20 13:53:59 -0500

    Initial commit

---
+++ src/com/branden/Main.java
@@ -0,0 +1,9 @@
+package com.branden;
+
+// Ask user for a name of a movie, search Open Movie Database, get Rotten Tomatoes rating
+public class Main {
+
+    public static void main(String[] args) {
+                new Movies();
+    }
+}
---
+++ src/com/branden/Movies.java
@@ -0,0 +1,34 @@
+package com.branden;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Movies extends JFrame{
+    private JLabel enterMovieNameLabel;
+    private JTextField movieNameTextField;
+    private JLabel resultsLabel;
+    private JPanel rootPanel;
+    private JButton searchButton;
+
+    Movies(){
+        super("Movie Ratings");
+        setContentPane(rootPanel);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pack();
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = movieNameTextField.getText();
+                if ( text.length() > 2){
+                    System.out.println(text);
+                } else {
+                    resultsLabel.setText("Must enter a movie name longer than 2 characters");
+                    pack();
+                }
+            }
+        });
+    }
+}
