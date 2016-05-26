162705298cb63a7b6ae36f84bc4b3cffec28b7f7
Branden <yp0975eu@go.minneapols.edu>
2016-05-26 16:53:18 -0500

    Added external libraries - retrofit2 and gson with maven. Configured url to call correctly

--- src/com/branden/Movies.java
+++ src/com/branden/Movies.java
@@ -9,19 +9,26 @@ public class Movies extends JFrame{
     private JLabel resultsLabel;
     private JPanel rootPanel;
     private JButton searchButton;
-
+    private Omdb omdb;
     Movies(){
+        // set display
         super("Movie Ratings");
         setContentPane(rootPanel);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
+        // set up retrofit class
+        omdb = new Omdb();
+        // event listeners
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String text = movieNameTextField.getText();
                 if ( text.length() > 2){
-                    System.out.println(text);
+
+
+                   omdb.getMovieByName(text);
+
                 } else {
                     resultsLabel.setText("Must enter a movie name longer than 2 characters");
                     pack();
--- src/com/branden/Omdb.java
+++ src/com/branden/Omdb.java
@@ -1,7 +1,63 @@
 package com.branden;
 
+import com.google.gson.Gson;
+import okhttp3.ResponseBody;
+import retrofit2.*;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class for interacting with Open Movie Database http://omdbapi.com/
  */
 public class Omdb {
+    private String baseURL="http://www.omdbapi.com";
+    private Retrofit retrofit;
+    private OmdbService service;
+    Omdb(){
+        // instantiate new retrofit class with our base url for omdbapi.com
+        retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = retrofit.create(OmdbService.class);
+
+    }
+    public interface OmdbService{
+        @GET("/")
+        Call<Gson> getMovie(@QueryMap Map<String, String> options);
+
+    }
+    public void getMovieByName(String movieName){
+        // create map to pass to parameters to getMovie
+        HashMap<String, String> options = new HashMap<>();
+        options.put("t",movieName);
+        options.put("r","json");
+        options.put("y","");
+        options.put("plot","short");
+
+        Call<Gson> responseBodyCall =  service.getMovie( options);
+        try {
+
+            Response<Gson> response = responseBodyCall.execute();
+            if( !response.isSuccessful() ) {
+                System.out.println(response.code());
+                System.out.println(response.raw());
+                System.out.println(response.message());
+              } else {
+                System.out.println(response.raw());
+                // mystery output
+                //Gson body = response.body();
+            }
+        } catch (IOException ex){
+            System.out.println(ex); ex.printStackTrace();
+        }
+
+    }
+
 }
+
+
